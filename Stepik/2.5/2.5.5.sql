cbd11141cdb3adc14c4e01564552617ad24921e5
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-03-19 20:03:11 +0300

    Create 2.5.5.sql

---
+++ Stepik/2.5/2.5.5.sql
@@ -0,0 +1,22 @@
+--2.5.5
+select * from buy;
+select * from buy_book;
+select * from book;
+/*Уменьшить количество тех книг на складе, которые были включены в заказ с номером 5.*/
+update book
+set amount = amount -
+             iif(book_id = 8,
+                (select amount
+                from buy_book
+                where buy_id = 5 and book_id = 8),
+                    (select amount
+                    from buy_book
+                    where buy_id = 5 and book_id = 2))
+where book_id in (8, 2);
+select * from book;
+
+update bk
+set amount = amount - bb.amount
+from book bk
+inner join buy_book bb on bb.book_id = bk.book_id
+where bb.buy_id = 5;
