bf474106ff0f0b2e3285a0b4c03e903e9d4fb77e
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-03-22 23:21:10 +0300

    Update 3.1.3.sql

--- Stepik/3.1/3.1.3.sql
+++ Stepik/3.1/3.1.3.sql
@@ -3,10 +3,12 @@
 который округлить до 2 знаков после запятой. Под результатом попытки понимается процент правильных
 ответов на вопросы теста, который занесен в столбец result.  В результат включить название дисциплины,
 а также вычисляемые столбцы Количество и Среднее. Информацию вывести по убыванию средних результатов.*/
-select name_subject, count(student_id) as Количество, ROUND(avg(result),2) as Среднее
+select name_subject, count(student_id) as Количество, ROUND(avg(result), 2) as Среднее
 from subject
-left join attempt a on subject.subject_id = a.subject_id
+         left join attempt a on subject.subject_id = a.subject_id
 group by name_subject;
 
-select * from attempt;
-select * from subject;
+select *
+from attempt;
+select *
+from subject;
