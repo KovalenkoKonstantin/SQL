82295f794f26d7fb21bb32d7db03f55d19dfee58
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-03-28 23:18:37 +0300

    Update 3.4.3.sql

--- Stepik/3.4/3.4.3.sql
+++ Stepik/3.4/3.4.3.sql
@@ -4,8 +4,8 @@
   хотя бы по одному предмету (использовать запрос из предыдущего урока).*/
 delete
 from applicant
-where (program_id, enrollee_id) in
-      (select p.program_id, enrollee.enrollee_id
+where program_id in
+      (select p.program_id
        from enrollee
                 inner join program_enrollee pe on enrollee.enrollee_id = pe.enrollee_id
                 inner join program p on p.program_id = pe.program_id
@@ -14,7 +14,18 @@ where (program_id, enrollee_id) in
                 inner join enrollee_subject es on es.subject_id = s.subject_id
            and es.enrollee_id = enrollee.enrollee_id
        where result < min_result
-       group by p.program_id, enrollee.enrollee_id);
+       group by p.program_id)
+  and enrollee_id in
+      (select enrollee.enrollee_id
+       from enrollee
+                inner join program_enrollee pe on enrollee.enrollee_id = pe.enrollee_id
+                inner join program p on p.program_id = pe.program_id
+                inner join program_subject ps on p.program_id = ps.program_id
+                inner join subject s on s.subject_id = ps.subject_id
+                inner join enrollee_subject es on es.subject_id = s.subject_id
+           and es.enrollee_id = enrollee.enrollee_id
+       where result < min_result
+       group by enrollee.enrollee_id);
 
 /*delete from applicant USING applicant /*For MYSQL*/
 inner join program_subject using(program_id)
