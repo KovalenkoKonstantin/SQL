c83ae9453cd1a6434552c3a317862bded6917018
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-07-03 11:20:36 +0300

    Create company.sql

---
+++ Excel/company.sql
@@ -0,0 +1,42 @@
+drop procedure if exists CompanyCreation;
+create procedure CompanyCreation as
+begin
+    create table company
+    (
+        company_id        int primary key identity (1,1),
+        company_name      varchar(30),
+        company_full_name varchar(100)
+    )
+end;
+    execute CompanyCreation;
+
+    drop procedure if exists CompanyInsertion;
+    create procedure CompanyInsertion as
+    begin
+        insert into company(company_name, company_full_name)
+        values ('АО "ИнфоТеКС"',
+                'Акционерное общество «Информационные технологии и коммуникационные системы» (АО «ИнфоТеКС»)'),
+               ('АО "ПМ"', 'Акционерное общество  «Перспективный мониторинг» (АО «ПМ»)'),
+               ('ООО "СФБ Лаб"', 'Общество с ограниченной ответственностью «СФБ Лаборатория» (ООО «СФБ Лаб»)')
+    end;
+        execute CompanyInsertion;
+
+    select *
+    from company;
+
+    insert into company(company_name, company_full_name)
+    values ('ООО "СФБ Лаб"', 'Общество с ограниченной ответственностью «СФБ Лаборатория» (ООО «СФБ Лаб»)');
+
+    delete
+    from company
+    where company_id = 3;
+
+    update company
+    set company_full_name = ('Общество с ограниченной ответственностью «СФБ Лаборатория» (ООО «СФБ Лаб»)')
+    where company_id = 3;
+
+    drop table if exists company;
+
+
+
+
