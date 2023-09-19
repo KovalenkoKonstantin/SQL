948a860dc14e40e2210b226ee57f79a7b5b1627b
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-09-19 08:42:05 +0300

    Update Components.sql

--- Components.sql
+++ Components.sql
@@ -82,3 +82,41 @@ values (68, 'Компактный компьютер Lanner NCA-1010 Intel Atom 
 
 select *
 from Components;
+
+select component_name,
+       component_unit,
+       component_quantity,
+       component_price,
+       component_document,
+       contractor_full_name,
+       contractor_inn
+from Components
+         inner join Contractors C on Components.contractor_id = C.contractor_id
+         inner join Project P on Components.genuine_project_id = P.genuine_project_id
+where project_cipher like
+      'Программно-аппаратный комплекс ViPNet Coordinator HW50 A 4.x (+3G)(+unlim%';
+
+drop procedure if exists ComponentsRefresh;
+create procedure ComponentsRefresh @cipher as nvarchar(150)
+as
+begin
+    --prevent the "1 row affected" message from being returned for every operation
+    set nocount on
+--statement for the procedure
+    select component_name,
+           component_unit,
+           component_quantity,
+           component_price,
+           component_document,
+           contractor_full_name,
+           contractor_inn
+    from Components
+             inner join Contractors C on Components.contractor_id = C.contractor_id
+             inner join Project P on Components.genuine_project_id = P.genuine_project_id
+    where project_cipher like
+          --'Программно-аппаратный комплекс ViPNet Coordinator HW50 A 4.x (+3G)(+unlim%';
+    @cipher;
+end
+go
+
+exec ComponentsRefresh 'Программно-аппаратный комплекс ViPNet Coordinator HW50 A 4.x (+3G)(+u%';
