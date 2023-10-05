e90074a3985fbb545f33c13ee97fe908b1e4b7ba
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-10-05 17:58:18 +0300

    Create service.sql

---
+++ service.sql
@@ -0,0 +1,31 @@
+select employee_position as "Должность", schedule_description as "График работы", employee_name as Сотрудник
+from EmployeeChanges
+inner join Employee E on EmployeeChanges.tab_N = E.tab_N;
+
+select project_id, project_cipher from Project
+where project_cipher like 'Улей-23 Этап%';
+
+insert into AccrualType(accrual_type)
+values (N'ОЗП'),
+       (N'ДЗП'),
+       (N'Прочие'),
+       (N'База');
+
+alter table Salary
+    add accrual_id int;
+
+update Salary
+set accrual_id =
+        (
+            select accrual_id
+            from AccrualType
+            where Salary.accrual_type = AccrualType.accrual_type
+        );
+
+update SalaryBudget
+set accrual_id =
+        (
+            select accrual_id
+            from AccrualType
+            where SalaryBudget.accrual_type = AccrualType.accrual_type
+        );
