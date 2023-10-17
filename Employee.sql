86c15478ed1411035a511647aa1bf467ac4a4ae6
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2023-10-17 13:54:46 +0300

    Update Employee.sql

--- Employee.sql
+++ Employee.sql
@@ -49,3 +49,49 @@ alter table Employee alter column tab_N varchar(10) not null;
 alter table Employee
 	add constraint PK_Employee_tab_N
 		primary key nonclustered (tab_N);
+
+update Employee
+set employee_name_trimmed = (
+    select employee_name
+    from
+    Employee
+    where employee_name = employee_name_trimmed
+    );
+alter table Employee
+    drop column employee_name_trimmed
+
+drop function if exists trim;
+CREATE FUNCTION TRIM(@string VARCHAR(max))
+    RETURNS VARCHAR(max)
+     BEGIN
+      RETURN LTRIM(RTRIM(@string))
+     END
+    GO
+
+SELECT compatibility_level FROM sys.databases;
+
+ALTER DATABASE RKM
+SET COMPATIBILITY_LEVEL = 140;
+
+update Employee set employee_name_trimmed = TRIM(employee_name_trimmed);
+select TRIM(employee_name_trimmed) from Employee;
+SELECT rTRIM (employee_name) AS Trimmed from Employee;
+
+select rtrim(employee_name) as employee_name, month_name, year_number,
+       accrual_type, salary_bugget_ammount from SalaryBudget
+inner join AccrualType A on SalaryBudget.accrual_id = A.accrual_id
+inner join Employee E on SalaryBudget.tab_N = E.tab_N
+inner join Month M on SalaryBudget.month_id = M.month_id
+inner join Year Y on SalaryBudget.year_id = Y.year_id
+
+where employee_name <> '' --and accrual_type = 'ОКЛАД' or accrual_type = 'НАДБАВКА'
+--and year_number = 2023 and month_name = 'Сентябрь'
+order by employee_name;
+
+select rtrim(employee_name) as employee_name, month_name, year_number,
+       employee_accounting_type, employee_position from EmployeeChanges
+inner join Employee E on EmployeeChanges.tab_N = E.tab_N
+inner join Month M on EmployeeChanges.month_id = M.month_id
+inner join Year Y on EmployeeChanges.year_id = Y.year_id
+where employee_name <> '' and year_number > 2022
+order by employee_name;
