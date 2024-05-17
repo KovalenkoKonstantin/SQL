fecd2678adc67682babf455cd38e48204a94daea
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-05-17 13:46:47 +0300

    Update

--- Employee.sql
+++ Employee.sql
@@ -1,19 +1,20 @@
-select tab_N from Employee
+select tab_N
+from Employee
 where employee_name = 'Авраменко Юрий Вячеславович';
 
 alter table Employee
     drop constraint FK_LabourIntensity_project_id;
 
 alter table Employee
-	add constraint FK_LabourIntensity_project_id
-		foreign key (project_id) references Project (project_id);
+    add constraint FK_LabourIntensity_project_id
+        foreign key (project_id) references Project (project_id);
 
 alter table Employee
-	add organization_id int;
+    add organization_id int;
 
 alter table Employee
-	add constraint FK_Employee_organization_id
-		foreign key (organization_id) references Organization (organization_id);
+    add constraint FK_Employee_organization_id
+        foreign key (organization_id) references Organization (organization_id);
 
 update Employee
 set organization_id =
@@ -29,80 +30,98 @@ alter table Employee
 alter table Employee
     drop column organization_inn;
 
-select * from Employee;
+select *
+from Employee;
 
 alter table Employee
     drop constraint FK_Employee_organization_id;
 
 alter table Employee
-	add constraint FK_Employee_organization_id
-		foreign key (organization_id) references Organization (organization_id);
+    add constraint FK_Employee_organization_id
+        foreign key (organization_id) references Organization (organization_id);
 
-select * from Employee;
+select *
+from Employee;
 
 alter table Employee
     drop constraint PK_Employee
 go
 
-alter table Employee alter column tab_N varchar(10) not null;
+alter table Employee
+    alter column tab_N varchar(10) not null;
 
 alter table Employee
-	add constraint PK_Employee_tab_N
-		primary key nonclustered (tab_N);
+    add constraint PK_Employee_tab_N
+        primary key nonclustered (tab_N);
 
 update Employee
 set employee_name_trimmed = (
     select employee_name
-    from
-    Employee
+    from Employee
     where employee_name = employee_name_trimmed
-    );
+);
 alter table Employee
     drop column employee_name_trimmed
 
 drop function if exists trim;
 CREATE FUNCTION TRIM(@string VARCHAR(max))
     RETURNS VARCHAR(max)
-     BEGIN
-      RETURN LTRIM(RTRIM(@string))
-     END
-    GO
+BEGIN
+    RETURN LTRIM(RTRIM(@string))
+END
+GO
 
-SELECT compatibility_level FROM sys.databases;
+SELECT compatibility_level
+FROM sys.databases;
 
 ALTER DATABASE RKM
-SET COMPATIBILITY_LEVEL = 140;
+    SET COMPATIBILITY_LEVEL = 140;
 
-update Employee set employee_name_trimmed = TRIM(employee_name_trimmed);
-select TRIM(employee_name_trimmed) from Employee;
-SELECT rTRIM (employee_name) AS Trimmed from Employee;
+update Employee
+set employee_name_trimmed = TRIM(employee_name_trimmed);
+select TRIM(employee_name_trimmed)
+from Employee;
+SELECT rTRIM(employee_name) AS Trimmed
+from Employee;
 
-select rtrim(employee_name) as employee_name, month_name, year_number,
-       accrual_type, salary_bugget_ammount from SalaryBudget
-inner join AccrualType A on SalaryBudget.accrual_id = A.accrual_id
-inner join Employee E on SalaryBudget.tab_N = E.tab_N
-inner join Month M on SalaryBudget.month_id = M.month_id
-inner join Year Y on SalaryBudget.year_id = Y.year_id
+select rtrim(employee_name) as employee_name,
+       month_name,
+       year_number,
+       accrual_type,
+       salary_bugget_ammount
+from SalaryBudget
+         inner join AccrualType A on SalaryBudget.accrual_id = A.accrual_id
+         inner join Employee E on SalaryBudget.tab_N = E.tab_N
+         inner join Month M on SalaryBudget.month_id = M.month_id
+         inner join Year Y on SalaryBudget.year_id = Y.year_id
 
 where employee_name <> '' --and accrual_type = 'ОКЛАД' or accrual_type = 'НАДБАВКА'
 --and year_number = 2023 and month_name = 'Сентябрь'
 order by employee_name;
 
-select tab_N, rtrim(employee_name) as employee_name, month_name, year_number,
-       employee_position from EmployeeChanges
-inner join Employee E on EmployeeChanges.tab_N = E.tab_N
-inner join Month M on EmployeeChanges.month_id = M.month_id
-inner join Year Y on EmployeeChanges.year_id = Y.year_id
-where employee_name = 'Бушма Роман Александрович' and year_number > 2022
+select tab_N,
+       rtrim(employee_name) as employee_name,
+       month_name,
+       year_number,
+       employee_position
+from EmployeeChanges
+         inner join Employee E on EmployeeChanges.tab_N = E.tab_N
+         inner join Month M on EmployeeChanges.month_id = M.month_id
+         inner join Year Y on EmployeeChanges.year_id = Y.year_id
+where employee_name = 'Бушма Роман Александрович'
+  and year_number > 2022
 order by month_name;
 
-select tab_N from Employee
+select tab_N
+from Employee
 where employee_name = 'Бушма Роман Александрович';
 
-select employee_name from Employee
+select employee_name
+from Employee
 where tab_N = '000001762 '
 
-select employee_position from EmployeeChanges
+select employee_position
+from EmployeeChanges
 where tab_N = '000001762 '
 
 update EmployeeChanges
@@ -113,159 +132,179 @@ update Employee
 set employee_name = 'Бушма Роман Александрович Старый'
 where tab_N = '000001762 ';
 
-select rtrim(employee_name) as employee_name, tab_N from Employee;
+select rtrim(employee_name) as employee_name, tab_N
+from Employee;
 
-select tab_N from Employee
+select tab_N
+from Employee
 where rtrim(employee_name) like 'Стенечкина Елена Александровна';
 
-select rtrim(employee_name) as employee_name, tab_N from Employee
+select rtrim(employee_name) as employee_name, tab_N
+from Employee
 where employee_name <> ''
-and organization_id = 3
+  and organization_id = 3
 order by employee_name;
 
 drop procedure if exists GetEmployeeRefresh;
-create procedure GetEmployeeRefresh
-@index as integer
+create procedure GetEmployeeRefresh @index as integer
 as
 begin
---prevent the "1 row affected" message from being returned for every operation
+    --prevent the "1 row affected" message from being returned for every operation
     set nocount on
 --statement for the procedure
-select rtrim(employee_name) as employee_name, tab_N from Employee
-where employee_name <> ''
-and organization_id = @index
-order by employee_name
+    select rtrim(employee_name) as employee_name, tab_N
+    from Employee
+    where employee_name <> ''
+      and organization_id = @index
+    order by employee_name
 end
 go
 
 exec GetEmployeeRefresh;
 
-select employee_name, tab_N from Employee
+select employee_name, tab_N
+from Employee
 where tab_N = '0000000162';
 
-select employee_name, tab_N from Employee
-where organization_id = 9 and employee_name = '';
+select employee_name, tab_N
+from Employee
+where organization_id = 9
+  and employee_name = '';
 
 drop procedure if exists GetEmployee
-create procedure GetEmployee
-@org as int
+create procedure GetEmployee @org as int
 as
 begin
---prevent the "1 row affected" message from being returned for every operation
+    --prevent the "1 row affected" message from being returned for every operation
     set nocount on
 --statement for the procedure
-select employee_name, tab_N, format(date_of_dismissal, 'dd.MM.yyyy') as date_of_dismissal from Employee
+    select employee_name, tab_N, format(date_of_dismissal, 'dd.MM.yyyy') as date_of_dismissal
+    from Employee
 -- inner join Organization O on Employee.organization_id = O.organization_id
 -- where organization_name = 'АО "ПМ"'
-where organization_id = @org
+    where organization_id = @org
     order by tab_N desc
 end
+    exec GetEmployee 9;
 
-exec GetEmployee 9;
-
-select employee_name, tab_N from Employee
+select employee_name, tab_N
+from Employee
 where employee_name = 'Кислова Татьяна Константиновна';
 
-select * from Employee
-    where employee_name = 'Ганжа Юрий Сергеевич';
+select *
+from Employee
+where employee_name = 'Ганжа Юрий Сергеевич';
 
 
 select employee_name, tab_N, format(date_of_dismissal, 'dd.MM.yyyy') as date_of_dismissal
 from Employee
-where organization_id = 9 and employee_name = 'Родионов Феликс Николаевич'
-    order by employee_name;
-
-select * from Employee
-where employee_name like '%(%'
-
-select * from Employee
-where employee_name like 'Юркин Константин Юрьевич';
-
-select top 50 * from Employee
-    where employment !='';
-
-select * from Employee
-    where employee_name = 'Булавкин Даниил Сергеевич';
-
-select * from Employee
-    where tab_N like '0000000075'
-    order by employee_name;
-
-select * from Employee
-    where organization_id = 6
+where organization_id = 9
+  and employee_name = 'Родионов Феликс Николаевич'
 order by employee_name;
 
-drop index PK_Employee_tab_N on Employee
+select *
+from Employee
+where employee_name like '%(%'
+
+select *
+from Employee
+where employee_name like 'Юркин Константин Юрьевич';
+
+select top 50 *
+from Employee
+where employment != '';
+
+select *
+from Employee
+where employee_name = 'Булавкин Даниил Сергеевич';
+
+select *
+from Employee
+where tab_N like '0000000075'
+order by employee_name;
+
+select *
+from Employee
+where organization_id = 6
+order by employee_name;
+
+    drop index PK_Employee_tab_N on Employee
 go
 alter table Employee
     drop constraint PK_Employee_tab_N
 go
 alter table Employee
-	add GUID varchar(100)
+    add GUID varchar(100)
 go
-alter table Employee alter column GUID nvarchar(36) null
+alter table Employee
+    alter column GUID nvarchar(36) null
 go
 
 alter procedure GetEmployee
-@org as int,
-@startdate as date
-as
-begin
---prevent the "1 row affected" message from being returned for every operation
-    set nocount on
+    @org as int,
+        @startdate as date
+    as
+    begin
+        --prevent the "1 row affected" message from being returned for every operation
+        set nocount on
 --statement for the procedure
-select employee_name, tab_N, format(date_of_dismissal, 'dd.MM.yyyy') as date_of_dismissal from Employee
+        select employee_name, tab_N, format(date_of_dismissal, 'dd.MM.yyyy') as date_of_dismissal
+        from Employee
 -- inner join Organization O on Employee.organization_id = O.organization_id
 -- where organization_name = 'АО "ПМ"'
-where organization_id = @org
-and date_of_dismissal > @startdate
-or date_of_dismissal = '1753-01-01'
-    order by tab_N desc
-end
+        where organization_id = @org
+            and date_of_dismissal > @startdate
+           or date_of_dismissal = '1753-01-01'
+        order by tab_N desc
+    end
+    exec GetEmployee 3, '2022-06-01';
 
-exec GetEmployee 3, '2022-06-01';
-
-select * from Employee
-where tab_N = '0000000129'
-
-alter table Employee
-    drop constraint PK_Employee_tab_N
+    select *
+    from Employee
+    where tab_N = '0000000129'
+    alter table Employee
+        drop constraint PK_Employee_tab_N
 go
 
-select * from Employee
+select *
+from Employee
 where GUID is null
 --and employee_name <> ''
 order by employee_name;
 
-delete from Employee where GUID is null;
+delete
+from Employee
+where GUID is null;
 
-alter table Employee alter column GUID nvarchar(36) not null
+alter table Employee
+    alter column GUID nvarchar(36) not null
 go
 
 alter table Employee
-	add constraint PK_Employee_GUID
-		primary key nonclustered (GUID)
+    add constraint PK_Employee_GUID
+        primary key nonclustered (GUID)
 go
 
-select * from Employee
+select *
+from Employee
 where employee_name = 'Дескат Михаил Витальевич'
 
 exec GetEmployeeRefresh 9;
 
 drop procedure if exists GetEmployeeRefreshAlt;
-create procedure GetEmployeeRefreshAlt
-@index as integer
+create procedure GetEmployeeRefreshAlt @index as integer
 as
 begin
---prevent the "1 row affected" message from being returned for every operation
+    --prevent the "1 row affected" message from being returned for every operation
     set nocount on
 --statement for the procedure
-select rtrim(employee_name) as employee_name, tab_N from Employee
-where employee_name <> ''
-and organization_id = @index
-and date_of_dismissal = '1753-01-01'
-and fired = 0
-order by employee_name
+    select rtrim(employee_name) as employee_name, tab_N
+    from Employee
+    where employee_name <> ''
+      and organization_id = @index
+      and date_of_dismissal = '1753-01-01'
+      and fired = 0
+    order by employee_name
 end
 go
 
@@ -275,33 +314,87 @@ update Employee
 set city_id = 4;
 
 select employee_name,
-       replace(city_name, N'г. ', '') as city_name from Employee
-inner join City C on Employee.city_id = C.city_id
+       replace(city_name, N'г. ', '') as city_name
+from Employee
+         inner join City C on Employee.city_id = C.city_id
 where organization_id = 3;
 
 exec GetEmployeeRefresh 9;
 
 drop procedure if exists GetEmployeeListShchepetova;
-create procedure GetEmployeeListShchepetova
-@organization_id as integer
+create procedure GetEmployeeListShchepetova @organization_id as integer
 as
 begin
---prevent the "1 row affected" message from being returned for every operation
+    --prevent the "1 row affected" message from being returned for every operation
     set nocount on
 --statement for the procedure
-select employee_name,
-       replace(city_name, N'г. ', '') as city_name from Employee
-inner join City C on Employee.city_id = C.city_id
-where organization_id = @organization_id
-order by employee_name
+    select employee_name,
+           replace(city_name, N'г. ', '')               as city_name,
+           replace(employment_date, '1753-01-01', '')   as employment_date,
+           replace(date_of_dismissal, '1753-01-01', '') as date_of_dismissal
+    from Employee
+             inner join City C on Employee.city_id = C.city_id
+    where organization_id = @organization_id
+-- and employment_date <> '1753-01-01'
+    order by employee_name,
+             employment_date desc
 end
 go
 
 execute GetEmployeeListShchepetova 9;
 
-select employee_name, city_name, fired from Employee
-inner join City C on Employee.city_id = C.city_id
-where employee_name = 'Шалаев Илья Александрович'
+select employee_name, city_name, fired, employment_date
+from Employee
+         inner join City C on Employee.city_id = C.city_id
+where employee_name = 'Бреус Данил Евгеньевич'
 
-select employee_name, city_id from Employee
-where employee_name = 'Шалаев Илья Александрович'
+select employee_name, city_id
+from Employee
+where employee_name = 'Бреус Данил Евгеньевич'
+
+update Employee
+set city_id = 6
+where employee_name = 'Шкуратов Дмитрий Борисович'
+
+drop procedure if exists GetEmployeeRefreshAltДМС;
+create procedure GetEmployeeRefreshAltДМС
+    @organization_id as integer,
+    @date as date
+as
+begin
+    --prevent the "1 row affected" message from being returned for every operation
+    set nocount on
+    --statement for the procedure
+--     set @year_id = convert(date, @year_id, 101)
+    select rtrim(employee_name) as employee_name, tab_N
+    from Employee
+    where employee_name <> ''
+      and organization_id = @organization_id
+      and date_of_dismissal > @date
+    -- order by employee_name
+    UNION
+    select rtrim(employee_name) as employee_name, tab_N
+    from Employee
+    where employee_name <> ''
+      and organization_id = @organization_id
+      and fired = 0
+    order by employee_name
+end
+go
+
+execute GetEmployeeRefreshAltДМС 9, '2023-07-30';
+
+
+select rtrim(employee_name) as employee_name, tab_N
+from Employee
+where employee_name <> ''
+  and organization_id = 9
+  and date_of_dismissal > '2023-07-30'
+-- order by employee_name
+UNION
+select rtrim(employee_name) as employee_name, tab_N
+from Employee
+where employee_name <> ''
+  and organization_id = 9
+  and fired = 0
+order by employee_name
--- SalaryBudget.sql
+++ SalaryBudget.sql
@@ -163,11 +163,11 @@ inner join Employee E on SalaryBudget.GUID = E.GUID
 inner join Month M on SalaryBudget.month_id = M.month_id
 inner join Year Y on SalaryBudget.year_id = Y.year_id
 where employee_name <> ''
-and year_number >= @year_number
-and organization_id = @organization_id
--- and year_number >= 2024
--- and organization_id = 9
--- and date_of_dismissal = '1753-01-01'
+-- and year_number >= @year_number
+-- and organization_id = @organization_id
+and year_number >= 2024
+and organization_id = 9
+and employee_name = 'Бреус Данил Евгеньевич'
 and accrual_id = 1
 group by rtrim(employee_name), month_name, year_number, fired
 order by employee_name, year_number
@@ -233,6 +233,6 @@ execute GetSalaryList 9, 2024;
 execute GetSalaryListShchepetova 9, 2023;
 execute GetSalaryListShchepetovaAlt 04, 2024,9;
 execute GetSalaryListShchepetovaRK 9,2024;
-execute GetSalaryListShchepetovaGPC 9, 2023;
+execute GetSalaryListShchepetovaGPC 9, 2024;
 execute GetSalaryBudgetRefresh 9, 2024;
 
