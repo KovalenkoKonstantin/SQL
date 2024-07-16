94d55318145242535c8a844f567faae988da52e8
Kovalenko Konstantin <94693119+KovalenkoKonstantin@users.noreply.github.com>
2024-07-16 10:25:09 +0300

    added comments and fixes(a range of conditions specified) in employee alternative list (extended version) changes procedure

--- Employee.sql
+++ Employee.sql
@@ -501,3 +501,30 @@ BEGIN
         year_number;                                     -- Сортировка по номеру года
 END
 GO
+
+-- Этот код создает хранимую процедуру,
+-- которая возвращает список сотрудников для заданной компании,
+-- которые не уволены, и сортирует их по табельному номеру в порядке убывания.
+-- Создание хранимой процедуры с именем GetEmployee
+CREATE PROCEDURE GetEmployee
+    @company_id AS INT  -- Входной параметр: идентификатор компании
+AS
+BEGIN
+    -- Отключение вывода сообщения "1 row affected" для каждой операции
+    SET NOCOUNT ON;
+
+    -- Основной запрос для процедуры
+    SELECT
+        employee_name,  -- Имя сотрудника
+        tab_N           -- Табельный номер сотрудника
+    FROM
+        Employee
+    -- INNER JOIN Organization O ON Employee.organization_id = O.organization_id
+    -- WHERE organization_name = 'АО "ПМ"'
+    WHERE
+        organization_id = @company_id  -- Фильтрация по идентификатору компании
+        AND fired = 0                  -- Фильтрация по статусу увольнения (0 - не уволен)
+    ORDER BY
+        tab_N DESC;  -- Сортировка по табельному номеру в порядке убывания
+END
+GO
